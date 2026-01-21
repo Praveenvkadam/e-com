745a89286b024bfc69cb534edef4ffe87b0e96c5
Praveen Kadam <93198500+Praveenvkadam@users.noreply.github.com>
2026-01-21 20:37:28 +0530

    feat: implement main application rendering and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
-import React from 'reac
-  return (
-    <div>App</div>
-  )
-}
+import { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-export default App
+import Sign from "./auth/Signup";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+
+import { startAuthListener } from "./redux/Listener/authListener";
+import { handleGoogleRedirectResult } from "./Service/auth"; 
+
+const App = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = startAuthListener(dispatch);
+    return () => unsubscribe();
+  }, [dispatch]);
+
+  useEffect(() => {
+    const run = async () => {
+      try {
+        const res = await handleGoogleRedirectResult();
+        if (res?.user) {
+          navigate("/home");
+        }
+      } catch (err) {
+        console.log("Google redirect error:", err);
+      }
+    };
+
+    run();
+  }, [navigate]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Sign />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
+};
+
+export default App;
